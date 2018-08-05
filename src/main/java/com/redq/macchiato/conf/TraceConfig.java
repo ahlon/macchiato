50cf046aa71240ef11748d32b4c0fa7f80f35c32
ahlon <ahlon2002@gmail.com>
2018-08-05 16:37:19 +0800

    rename trace config 

--- src/main/java/com/redq/macchiato/conf/TraceConfig.java
+++ src/main/java/com/redq/macchiato/conf/TraceConfig.java
@@ -2,12 +2,13 @@ package com.redq.macchiato.conf;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.redq.macchiato.interceptor.TraceInterceptor;
 
 @Configuration
-public class LoggerConfiguration extends WebMvcConfigurerAdapter {
+public class TraceConfig implements WebMvcConfigurer {
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new TraceInterceptor()).addPathPatterns("/**");
